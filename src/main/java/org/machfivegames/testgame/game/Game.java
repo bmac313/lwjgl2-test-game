a8d8ef8f2276f0c654929c86f82bef494d3ca262
Brendan <mcb0193@comcast.net>
2018-04-25 17:30:06 -0500

    Initial commit

---
+++ src/main/java/org/machfivegames/testgame/game/Game.java
@@ -0,0 +1,78 @@
+package main.java.org.machfivegames.testgame.game;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Game {
+
+    public void start() {
+        try {
+            Display.setDisplayMode(new DisplayMode(800, 600));
+            Display.create();
+        } catch(LWJGLException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
+        while(!Display.isCloseRequested()) {
+            pollInput();
+            Display.update();
+        }
+
+        Display.destroy();
+    }
+
+    private void pollInput() {
+        if (Mouse.isButtonDown(0)) {
+            int x = Mouse.getX();
+            int y = Mouse.getY();
+
+            System.out.println("MOUSE DOWN @ X: " + x + " Y: " + y);
+        }
+
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            System.out.println("SPACE KEY IS DOWN");
+        }
+
+        while (Keyboard.next()) {
+            if (Keyboard.getEventKeyState()) {
+                if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+                    System.out.println("A Key Pressed");
+                }
+                if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+                    System.out.println("S Key Pressed");
+                }
+                if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+                    System.out.println("D Key Pressed");
+                }
+                if (Keyboard.getEventKey() == Keyboard.KEY_W) {
+                    System.out.println("W Key Pressed");
+                }
+            } else {
+                if (Keyboard.getEventKey() == Keyboard.KEY_A) {
+                    System.out.println("A Key Released");
+                }
+                if (Keyboard.getEventKey() == Keyboard.KEY_S) {
+                    System.out.println("S Key Released");
+                }
+                if (Keyboard.getEventKey() == Keyboard.KEY_D) {
+                    System.out.println("D Key Released");
+                }
+                if (Keyboard.getEventKey() == Keyboard.KEY_W) {
+                    System.out.println("W Key Released");
+                }
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+
+}
